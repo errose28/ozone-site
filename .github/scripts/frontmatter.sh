1480962085909c6cbc2bd83f6b4822296ff7a655
Ethan Rose <erose@apache.org>
2024-02-06 23:38:46 -0800

    Make frontmatter script return1 on error

--- .github/scripts/frontmatter.sh
+++ .github/scripts/frontmatter.sh
@@ -2,6 +2,7 @@
 
 root="$(git rev-parse --show-toplevel)"
 schema="$root"/.github/resource/frontmatter.schema.json
+rc=0
 
 # Used to hold extracted frontmatter since ajv cannot read input from stdin.
 frontmatter_file="$(mktemp).json"
@@ -10,7 +11,11 @@ for file in $(find "$root"/docs -type f -iname '*.md'); do
   # Workaround since yq seems to have issues with files that do not have frontmatter defined.
   if yq --front-matter=extract --output-format=json "$file" 2>/dev/null 1>"$frontmatter_file"; then
     # Ignore stdout which always prints whether validation passed.
-    pnpm ajv validate --errors=text -s "$schema" -d "$frontmatter_file" 1>/dev/null || echo "^ Validation failed for $file"
+    if ! pnpm ajv validate --errors=text -s "$schema" -d "$frontmatter_file" 1>/dev/null; then
+      echo "^ Validation failed for $file"
+      rc=1
+    fi
   fi
 done
 
+exit "$rc"
