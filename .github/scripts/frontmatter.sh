79147a9a1d50fc5e56740f49587e4fb1e85ed3af
Ethan Rose <erose@apache.org>
2024-02-07 13:43:31 -0800

    Update messages and on frontmatter check

--- .github/scripts/frontmatter.sh
+++ .github/scripts/frontmatter.sh
@@ -12,10 +12,14 @@ for file in $(find "$root"/docs -type f -iname '*.md'); do
   if yq --front-matter=extract --output-format=json "$file" 2>/dev/null 1>"$frontmatter_file"; then
     # Ignore stdout which always prints whether validation passed.
     if ! pnpm ajv validate --errors=text -s "$schema" -d "$frontmatter_file" 1>/dev/null; then
-      echo "^ Validation failed for $file"
+      echo "^ Frontmatter validation failed for $file" 1>&2
       rc=1
     fi
   fi
 done
+
+if [ "$rc" != 0 ]; then
+  echo "\nThere were frontmatter validation failures against the schema $schema" 1>&2
+fi
 
 exit "$rc"
